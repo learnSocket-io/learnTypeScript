db950116cd365fb59c0a5a6915cf3079efbfa978
origin <yesk31@naver.com>
2023-01-17 07:22:52 +0900

    learn: 타입을 추론하는 객체에 대한 이해

--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
-var person = {
-    name: "hoho",
-    age: 30
+const person: {
+    name: string;
+    age : number
+} = {
+    name : "hoho",
+    age : 30,
 };
-console.log(person.name);
+
+console.log(person.name)
+
+
 // 일반적인 객체의 형태.
 // const person: {} = {     // ": {}" 객체를 생성하는것이 아닌 객체라고 타입을 지정하는 것.
 //     name : "hoho",
 //     age : 30,
 // };
+
 //두 타입의 객체가 비슷해 보일 수 있으나 문법적으로 알아두면 좋다고 생각한다.
+
 // 타입을 추론한 객체의 형태.
 // const person: {
 //     name : string;
