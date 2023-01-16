70284eb69e16ae7346f9c3323e2e1d40749b1222
origin <yesk31@naver.com>
2023-01-17 06:14:09 +0900

    기본 예

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log(`your code is change again.`)
+function add(num1, num2){
+    return num1 + num2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+
+console.log(result);
