ac6865019758633c58a0499b4847deb6aa7c1d40
origin <yesk31@naver.com>
2023-01-18 04:09:32 +0900

    learn: class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log("hello")
+class Department {
+  name: string;
+  employees:string[] = [];
+  constructor(n: string) {
+    this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log('Department: ' + this.name);
+  }
+  addEmployees(employee: string){
+    this.employees.push(employee);
+  }
+
+}
+
+const accounting = new Department('Accounting');
+
+accounting.describe();
+
+const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+accountingCopy.describe();
