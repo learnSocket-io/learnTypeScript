ac36dddf9277fa3cc832d977f9c635a536d649bc
origin <yesk31@naver.com>
2023-01-18 03:26:50 +0900

    test: class 준비

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,1 @@
-"use strict";
-function combine(input1, input2) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-}
-const combineResult = combine(30, 50);
-console.log(combineResult);
-const combineName = combine("Max", "Is");
-console.log(combineName);
+
